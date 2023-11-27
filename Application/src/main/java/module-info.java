7a6646378b097502aed0a5aeb51332faad5ae4e8
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-11-27 16:56:32 +0700

    rm module info

--- Application/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module hust.group.pttkhtaims {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens hust.group23.pttkhtaims to javafx.fxml;
-    exports hust.group23.pttkhtaims;
-}
