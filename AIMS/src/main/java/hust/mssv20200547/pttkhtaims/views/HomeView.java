85cbdb8362c510d79160fb5bf84ca4f11a2a27c1
bhtu <bhtu.work@gmail.com>
2024-01-03 10:30:00 +0700

    rm test, fix can't search
    fix control coupling
    delegate logic to service

--- AIMS/src/main/java/hust/mssv20200547/pttkhtaims/views/HomeView.java
+++ AIMS/src/main/java/hust/mssv20200547/pttkhtaims/views/HomeView.java
@@ -1,8 +1,6 @@
 package hust.mssv20200547.pttkhtaims.views;
 
 import hust.mssv20200547.pttkhtaims.controllers.HomeController;
-import hust.mssv20200547.pttkhtaims.database.IMediaSource;
-import hust.mssv20200547.pttkhtaims.database.mysql.MediaSourceMySql;
 
 import java.io.IOException;
 import java.net.URL;
@@ -10,7 +8,6 @@ import java.sql.SQLException;
 
 public class HomeView extends BaseView {
     private static final URL PATH = HomeView.class.getResource("/fxml/home-screen-view.fxml");
-    private static final IMediaSource MYSQL = new MediaSourceMySql();
 
     /**
      * Constructor
@@ -20,9 +17,6 @@ public class HomeView extends BaseView {
      */
     public HomeView() throws IOException, SQLException {
         super(PATH);
-
-        HomeController controller = this.loader.getController();
-        controller.setMedias(MYSQL.searchMedias("category", "", 20));
     }
 
     @Override
